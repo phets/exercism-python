30fbbc824fd5527535cfafe3c761dbc090f15ee4
Fabio Regis <fregis@ROTORSIM.IT>
2023-01-10 11:49:31 +0100

    Completed grains exercise.

--- exercises/practice/grains/grains.py
+++ exercises/practice/grains/grains.py
@@ -1,6 +1,19 @@
 def square(number):
-    pass
+    # when the square value is not in the acceptable range
+    if(number < 1 or number > 64):
+        raise ValueError("square must be between 1 and 64")
+
+    # With bit-shift operator
+    # return 1 << (number - 1)
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    total_grains = 0
+
+    for cell in range(1,65):
+        total_grains += square(cell)
+    
+    # With bit-shift operator
+    # return ( 1 << 64 ) - 1
+    return total_grains
