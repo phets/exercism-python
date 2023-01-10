381ce1987a47e0b4745c9f33e897be60322b8f92
Fabio Regis <fregis@ROTORSIM.IT>
2023-01-10 10:36:48 +0100

    Removed TODO comments and pass.

--- exercises/concept/guidos-gorgeous-lasagna/lasagna.py
+++ exercises/concept/guidos-gorgeous-lasagna/lasagna.py
@@ -3,14 +3,9 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
-
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
 
-# TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining(elapsed_bake_time: int):
     """Calculate the bake time remaining.
 
@@ -24,12 +19,6 @@ def bake_time_remaining(elapsed_bake_time: int):
 
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-    pass
-
-
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
-
 def preparation_time_in_minutes(number_of_layers: int):
     """Calculate the preparation time in minutes.
 
@@ -42,7 +31,6 @@ def preparation_time_in_minutes(number_of_layers: int):
     """
 
     return number_of_layers * PREPARATION_TIME
-# TODO: define the 'elapsed_time_in_minutes()' function
 
 def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int):
     """Calculate the time that has been spent preparing and cooking the lasagna.
@@ -57,3 +45,4 @@ def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int):
     Calculated by preparation_time_in_minutes(number_of_layers) + elapsed_bake_time.
     """
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
+    
