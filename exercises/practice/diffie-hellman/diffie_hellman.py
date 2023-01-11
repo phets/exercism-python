4259f970047e5ac68bba881a801514dfcb032c4c
Fabio Regis <fregis@ROTORSIM.IT>
2023-01-11 11:52:47 +0100

    Completed diffie-hellman exercise.

--- exercises/practice/diffie-hellman/diffie_hellman.py
+++ exercises/practice/diffie-hellman/diffie_hellman.py
@@ -1,10 +1,15 @@
+import secrets
+
 def private_key(p):
-    pass
+    # secrets.randbelow(N) returns a random integer between 0 and N exclusive.
+    return secrets.randbelow(p - 2) + 2
 
 
 def public_key(p, g, private):
-    pass
+    # pow -> (g ** private) % p
+    return pow(g, private, p)
 
 
 def secret(p, public, private):
-    pass
+    # pow -> (public ** private) % p
+    return pow(public, private, p)
