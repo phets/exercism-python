0a60fae94a159fbe1af95b3eac9bcc1f49ba3ac0
Fabio Regis <fregis@ROTORSIM.IT>
2023-01-11 08:34:36 +0100

    Complete traingle exercise.

--- exercises/practice/triangle/triangle.py
+++ exercises/practice/triangle/triangle.py
@@ -1,10 +1,37 @@
+def is_triangle(sides):
+    sides.sort()
+    if 0 > len(sides) > 3 :
+        raise ValueError("Sides must be an array containing exactly 3 elements.")
+        return False
+    if sides[0] + sides[1] <= sides[2] :
+        return False
+    return True
+
 def equilateral(sides):
-    pass
+    if not is_triangle(sides) :
+        return False
+    if sides[0] == sides[1] == sides[2] :
+        return True
+    return False
 
 
 def isosceles(sides):
-    pass
+    if not is_triangle(sides) :
+        return False
+    if equilateral(sides) :
+        return True
+    if sides[0] == sides[1] != sides[2] :
+        return True
+    if sides[1] == sides[2] != sides[0] :
+        return True
+    if sides[0] == sides[2] != sides[1] :
+        return True
+    return False
 
 
 def scalene(sides):
-    pass
+    if not is_triangle(sides) :
+        return False
+    if sides[0] != sides[1] != sides[2] :
+        return True
+    return False
