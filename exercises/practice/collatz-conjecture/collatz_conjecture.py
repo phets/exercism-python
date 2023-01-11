605a4c862f68695ebd53a1f961696c7bcaa21e95
Fabio Regis <fregis@ROTORSIM.IT>
2023-01-11 11:09:04 +0100

    Completed Collatz Conjecture exercise.

--- exercises/practice/collatz-conjecture/collatz_conjecture.py
+++ exercises/practice/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
-def steps(number):
-    pass
+def steps(number: int) -> int:
+    if not ( isinstance(number, int) and number > 0 ):
+        raise ValueError('Only positive integers are allowed')
+    steps = 0
+    while number != 1:
+        number = do_step(number)
+        steps += 1
+    return steps
+
+def do_step(number: int) -> int:
+    if number % 2 == 0:
+        return number / 2
+    return 3 * number + 1
