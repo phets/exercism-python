29d1ea6af97d428860a46ad92b2d57909822467b
Fabio Regis <fregis@ROTORSIM.IT>
2023-01-12 09:19:46 +0100

    Completed raindrops exercise.

--- exercises/practice/raindrops/raindrops.py
+++ exercises/practice/raindrops/raindrops.py
@@ -1,2 +1,18 @@
-def convert(number):
-    pass
+def convert(number: int) -> str:
+
+    factor_by_3 = number % 3 == 0
+    factor_by_5 = number % 5 == 0
+    factor_by_7 = number % 7 == 0
+
+    output = ''
+
+    if factor_by_3:
+        output += 'Pling'
+    if factor_by_5:
+        output += 'Plang'
+    if factor_by_7:
+        output += 'Plong'
+    if not (factor_by_3 or factor_by_5 or factor_by_7):
+        output = number
+    
+    return str(output)
