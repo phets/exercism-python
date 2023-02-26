4f669723535363772a20c0c74d360ce22523b137
phets <fabioregis1980@gmail.com>
2023-02-26 09:06:52 +0100

    Completed chaitanas-colossal-coaster exercise.

--- exercises/concept/chaitanas-colossal-coaster/list_methods.py
+++ exercises/concept/chaitanas-colossal-coaster/list_methods.py
@@ -10,8 +10,11 @@ def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
     :param person_name: str - name of person to add to a queue.
     :return: list - the (updated) queue the name was added to.
     """
-
-    pass
+    if ticket_type == 1:
+        express_queue.append(person_name)
+        return express_queue
+    normal_queue.append(person_name)
+    return normal_queue
 
 
 def find_my_friend(queue, friend_name):
@@ -21,8 +24,7 @@ def find_my_friend(queue, friend_name):
     :param friend_name: str - name of friend to find.
     :return: int - index at which the friends name was found.
     """
-
-    pass
+    return queue.index(friend_name)
 
 
 def add_me_with_my_friends(queue, index, person_name):
@@ -33,8 +35,8 @@ def add_me_with_my_friends(queue, index, person_name):
     :param person_name: str - the name to add.
     :return: list - queue updated with new name.
     """
-
-    pass
+    queue.insert(index, person_name)
+    return queue
 
 
 def remove_the_mean_person(queue, person_name):
@@ -44,8 +46,8 @@ def remove_the_mean_person(queue, person_name):
     :param person_name: str - name of mean person.
     :return: list - queue update with the mean persons name removed.
     """
-
-    pass
+    queue.remove(person_name)
+    return queue
 
 
 def how_many_namefellows(queue, person_name):
@@ -55,8 +57,7 @@ def how_many_namefellows(queue, person_name):
     :param person_name: str - name you wish to count or track.
     :return: int - the number of times the name appears in the queue.
     """
-
-    pass
+    return queue.count(person_name)
 
 
 def remove_the_last_person(queue):
@@ -65,8 +66,7 @@ def remove_the_last_person(queue):
     :param queue: list - names in the queue.
     :return: str - name that has been removed from the end of the queue.
     """
-
-    pass
+    return queue.pop(-1)
 
 
 def sorted_names(queue):
@@ -75,5 +75,4 @@ def sorted_names(queue):
     :param queue: list - names in the queue.
     :return: list - copy of the queue in alphabetical order.
     """
-
-    pass
+    return sorted(queue)
