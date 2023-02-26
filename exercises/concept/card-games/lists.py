b8e6ea28ba30ff937f455de1e572b23ce0e87484
phets <fabioregis1980@gmail.com>
2023-02-26 08:39:27 +0100

    Completed card-games exercise.

--- exercises/concept/card-games/lists.py
+++ exercises/concept/card-games/lists.py
@@ -10,8 +10,7 @@ def get_rounds(number):
     :param number: int - current round number.
     :return: list - current round and the two that follow.
     """
-
-    pass
+    return [number, number+1, number+2]
 
 
 def concatenate_rounds(rounds_1, rounds_2):
@@ -21,8 +20,7 @@ def concatenate_rounds(rounds_1, rounds_2):
     :param rounds_2: list - second set of rounds played.
     :return: list - all rounds played.
     """
-
-    pass
+    return rounds_1 + rounds_2
 
 
 def list_contains_round(rounds, number):
@@ -32,8 +30,7 @@ def list_contains_round(rounds, number):
     :param number: int - round number.
     :return: bool - was the round played?
     """
-
-    pass
+    return number in rounds
 
 
 def card_average(hand):
@@ -42,8 +39,7 @@ def card_average(hand):
     :param hand: list - cards in hand.
     :return: float - average value of the cards in the hand.
     """
-
-    pass
+    return sum(hand) / len(hand)
 
 
 def approx_average_is_average(hand):
@@ -52,8 +48,10 @@ def approx_average_is_average(hand):
     :param hand: list - cards in hand.
     :return: bool - does one of the approximate averages equal the `true average`?
     """
-
-    pass
+    median = hand[len(hand)//2]
+    approx = ( hand[0] + hand[-1] ) / 2
+    average = card_average(hand)
+    return average in (median, approx)
 
 
 def average_even_is_average_odd(hand):
@@ -62,8 +60,9 @@ def average_even_is_average_odd(hand):
     :param hand: list - cards in hand.
     :return: bool - are even and odd averages equal?
     """
-
-    pass
+    even = hand[::2]
+    odd = hand[1::2]
+    return card_average(even) == card_average(odd)
 
 
 def maybe_double_last(hand):
@@ -72,5 +71,6 @@ def maybe_double_last(hand):
     :param hand: list - cards in hand.
     :return: list - hand with Jacks (if present) value doubled.
     """
-
-    pass
+    if hand[-1] == 11:
+        hand[-1] = 22
+    return hand
