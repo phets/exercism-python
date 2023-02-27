8c031ef05df711abd315bfbc8e26aa5779f3197b
phets <fabioregis1980@gmail.com>
2023-02-27 09:36:58 +0100

    Completed locomotive-engineer exercise.

--- exercises/concept/locomotive-engineer/locomotive_engineer.py
+++ exercises/concept/locomotive-engineer/locomotive_engineer.py
@@ -2,13 +2,13 @@
 
 
 # TODO: define the 'get_list_of_wagons' function
-def get_list_of_wagons():
+def get_list_of_wagons(*ids):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    return list(ids)
 
 
 # TODO: define the 'fixListOfWagons()' function
@@ -19,19 +19,19 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :parm missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    wagon1, wagon2, locomotive, *other_wagons = each_wagons_id
+    return [locomotive, *missing_wagons, *other_wagons, wagon1, wagon2]
 
 
 # TODO: define the 'add_missing_stops()' function
-def add_missing_stops():
+def add_missing_stops(route, **stops):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
-
+    return route | {'stops': list(stops.values())}
 
 # TODO: define the 'extend_route_information()' function
 def extend_route_information(route, more_route_information):
@@ -41,7 +41,7 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
+    return route | more_route_information
 
 
 # TODO: define the 'fix_wagon_depot()' function
@@ -51,4 +51,4 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    return list(map(list,zip(*wagons_rows)))
