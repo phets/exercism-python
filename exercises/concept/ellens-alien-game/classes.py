86fda2b93c56e094aea13261561cc9cb2d994a35
phets <fabioregis1980@gmail.com>
2023-02-27 10:47:25 +0100

    Completed ellens-alien-game exercise.

--- exercises/concept/ellens-alien-game/classes.py
+++ exercises/concept/ellens-alien-game/classes.py
@@ -19,7 +19,32 @@ class Alien:
     collision_detection(other): Implementation TBD.
     """
 
-    pass
+    total_aliens_created = 0
+
+
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.health = 3
+        Alien.total_aliens_created += 1
+
+
+    def hit(self):
+        self.health -= 1
+
+
+    def is_alive(self):
+        return self.health > 0
+
+
+    def teleport(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+
+    def collision_detection(self,other_object):
+        pass
 
 
 #TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+def new_aliens_collection(positions):
+    return [Alien(*position) for position in positions]
