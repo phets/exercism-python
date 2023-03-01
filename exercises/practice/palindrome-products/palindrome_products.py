db5f18852d7a62ba2a02bf5150dac5d6376403d8
phets <fabioregis1980@gmail.com>
2023-03-01 14:14:33 +0100

    Completed palindrome-factors exercise.

--- exercises/practice/palindrome-products/palindrome_products.py
+++ exercises/practice/palindrome-products/palindrome_products.py
@@ -1,3 +1,5 @@
+import time
+
 def largest(min_factor: int=0, max_factor: int=0) -> tuple:
     """Given a range of numbers, find the largest palindromes which
        are products of two numbers within that range.
@@ -11,14 +13,15 @@ def largest(min_factor: int=0, max_factor: int=0) -> tuple:
 
     largest_palindrome = 0
     largest_palindrome_factors = []
-    for product in range (min_factor ** 2, max_factor ** 2 + 1):
-        if is_palindrome(product) and product > largest_palindrome:
-            factors = find_factors(product, min_factor, max_factor)
-            if not factors:
-                continue
-            else:
-                largest_palindrome = product
-                largest_palindrome_factors = factors
+    for product in range(max_factor**2, min_factor**2-1,-1):
+            if is_palindrome(product):
+                factors = find_factors(product, min_factor, max_factor)
+                if not factors:
+                    continue
+                else:
+                    largest_palindrome = product
+                    largest_palindrome_factors = factors
+                    break
     if not largest_palindrome_factors:
         return (None,[])
     else:
@@ -36,16 +39,19 @@ def smallest(min_factor, max_factor):
     """
     validate(min_factor, max_factor)
 
-    smallest_palindrome =  max_factor ** 2
     smallest_palindrome_factors = []
-    for product in range (min_factor ** 2, max_factor ** 2 + 1):
-        if is_palindrome(product) and product < smallest_palindrome:
+    for product in range(min_factor**2, max_factor**2+1):
+        if is_palindrome(product):
+            start = time.time()
             factors = find_factors(product, min_factor, max_factor)
+            end = time.time()
+            print(f'find_factors({product},{min_factor},{max_factor}) took {end-start}')
             if not factors:
                 continue
             else:
                 smallest_palindrome = product
                 smallest_palindrome_factors = factors
+                break
     if not smallest_palindrome_factors:
         return (None,[])
     else:
@@ -56,15 +62,17 @@ def is_palindrome(number):
     return str(number) == str(number)[::-1]
 
 def validate(min_factor: int, max_factor: int) -> None:
-    if min_factor < max_factor: raise ValueError("min must be <= max")
+    if min_factor > max_factor: raise ValueError("min must be <= max")
 
 def find_factors(number: int, min_factor: int, max_factor: int) -> list:
     output = []
-    for factor_1 in range(min_factor,max_factor+1,1):
-        for factor_2 in range(min_factor,max_factor+1,1):
-            if factor_1 * factor_2 == number:
-                output.append([factor_1, factor_2])
+    factor_1 = min_factor
+    while factor_1 <= max_factor:
+        factor_2, rem = divmod(number,factor_1)
+        if rem == 0 and min_factor <= factor_2 <= max_factor:
+            output.append([factor_1, factor_2])
+        factor_1 += 1
     return output
 
-print(largest(15,15))
-print(smallest(1002,1003))
+print(largest(min_factor=1000, max_factor=9999))
+print(smallest(min_factor=1000, max_factor=9999))
