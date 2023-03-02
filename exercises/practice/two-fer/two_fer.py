443df1ecf0b9acb4428f4ea5d2c34d8851bdb83c
phets <fabioregis1980@gmail.com>
2023-03-02 03:52:31 +0100

    Completed two-fer exercise.

--- exercises/practice/two-fer/two_fer.py
+++ exercises/practice/two-fer/two_fer.py
@@ -1,2 +1,20 @@
-def two_fer(name):
-    pass
+"""
+This module solves the two_fer exercise from exercism.org.
+
+Functions:
+
+    two_fer(str) -> str
+"""
+
+def two_fer(name: str='you') -> str:
+    """
+    This function creates and returns a string like:
+    One for {name}, one for me.
+
+    Args:
+        name (str, optional): The name to insert in the string. Defaults to 'you'.
+
+    Returns:
+        str: One for {name}, one for me.
+    """
+    return f'One for {name}, one for me.'
