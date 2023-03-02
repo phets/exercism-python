fc3289e4ac246bccfeb5f15fb35ac39f893802d1
phets <fabioregis1980@gmail.com>
2023-03-02 11:45:30 +0100

    Completed pangram exercise.

--- exercises/practice/pangram/pangram.py
+++ exercises/practice/pangram/pangram.py
@@ -1,2 +1,22 @@
-def is_pangram(sentence):
-    pass
+"""
+Module to find whether a string is a pangram.
+"""
+
+import string
+
+def is_pangram(sentence: str) -> bool:
+    """
+    Function that determines if an input string is a pangram.
+    A pangram is a sentence that contains all the letters of the alphabet.
+
+    Args:
+        sentence (str): The sentence to be analyzed.
+
+    Returns:
+        bool: True if the sentence is a pangram.
+    """
+    if sentence.strip() == '':
+        return False
+    input_set = set(sentence.strip().lower())
+    alphabet = set(string.ascii_lowercase)
+    return alphabet.issubset(input_set)
