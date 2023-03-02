d60d3857dfce552994e39591003c194835206d2f
phets <fabioregis1980@gmail.com>
2023-03-02 03:23:34 +0100

    Completed resistor-color exercise.

--- exercises/practice/resistor-color/resistor_color.py
+++ exercises/practice/resistor-color/resistor_color.py
@@ -1,6 +1,21 @@
-def color_code(color):
-    pass
+from enum import Enum
+ 
+class ResistorColors(Enum):
+    BLACK = 0
+    BROWN = 1
+    RED = 2
+    ORANGE = 3
+    YELLOW = 4
+    GREEN = 5
+    BLUE = 6
+    VIOLET = 7
+    GREY = 8
+    WHITE = 9
+
+
+def color_code(color: str) -> int:
+    return ResistorColors[color.strip().upper()].value
 
 
 def colors():
-    pass
+    return [color.name.lower() for color in ResistorColors]
