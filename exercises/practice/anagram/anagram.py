ce0c74c0babb49f66f61354b64b8a5048e3aab2f
phets <fabioregis1980@gmail.com>
2023-03-03 11:23:20 +0100

    Completed anagram exercise.

--- exercises/practice/anagram/anagram.py
+++ exercises/practice/anagram/anagram.py
@@ -1,2 +1,22 @@
-def find_anagrams(word, candidates):
-    pass
+"""
+Module to solve the anagram exercise from exercism.org.
+"""
+
+def find_anagrams(word: str, candidates: list[str]) -> list[str]:
+    """
+    Selects the entries in the candidates list that are anagrams
+    of word.
+
+    Args:
+        word (str): The word for which the candidates must be an anagram.
+        candidates (list[str]): The list of anagram candidates.
+
+    Returns:
+        list[str]: The anagrams of word included in the candidates list.
+    """
+    return [
+        anagram
+        for anagram in candidates
+        if anagram.lower().strip() != word.lower().strip()
+        and sorted(anagram.lower().strip()) == sorted(word.lower().strip())
+    ]
