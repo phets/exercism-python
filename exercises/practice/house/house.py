cae60e6df7296892aa3e507f9306533e632a5a16
phets <fabioregis1980@gmail.com>
2023-03-21 16:46:28 +0100

    Completed house exercise.

--- exercises/practice/house/house.py
+++ exercises/practice/house/house.py
@@ -1,2 +1,41 @@
 def recite(start_verse, end_verse):
-    pass
+    
+    predicates = [
+        'the house that Jack built',
+        'the malt',
+        'the rat',
+        'the cat',
+        'the dog',
+        'the cow with the crumpled horn',
+        'the maiden all forlorn',
+        'the man all tattered and torn',
+        'the priest all shaven and shorn',
+        'the rooster that crowed in the morn',
+        'the farmer sowing his corn',
+        'the horse and the hound and the horn'
+    ]
+    
+    verbs = [
+        'lay in',
+        'ate',
+        'killed',
+        'worried',
+        'tossed',
+        'milked',
+        'kissed',
+        'married',
+        'woke',
+        'kept',
+        'belonged to'
+    ]
+    
+    output = []
+    
+    for verse in range(start_verse - 1,end_verse):
+        phrase = f'This is {predicates[verse]}'
+        for inside_verse in range(verse,0,-1):
+            phrase += f' that {verbs[inside_verse-1]} {predicates[inside_verse-1]}'
+        phrase += '.'
+        output.append(phrase)
+        
+    return output
